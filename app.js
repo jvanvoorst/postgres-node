544f5feb90e04992141b9064cd20dc9cd8c23c95
Justin <justin.vanvoorst@gmail.com>
2016-02-10 17:00:15 -0700

    done with sql

--- app.js
+++ app.js
@@ -2,16 +2,22 @@ var pg = require('pg');
 
 var conString = require('./connection.js');
 
+var date = new Date('2016, 1, 1');
+
+var query = "SELECT * FROM sierra_view.record_metadata INNER JOIN sierra_view.bib_view ON sierra_view.record_metadata.record_num = sierra_view.bib_view.record_num WHERE sierra_view.record_metadata.record_last_updated_gmt > $1 AND sierra_view.record_metadata.record_type_code = $2 AND sierra_view.bib_view.bcode3 = $3 ORDER BY sierra_view.record_metadata.record_last_updated_gmt ASC";
+
+console.log('getting records updated since: ' + date);
+
 var client = new pg.Client(conString);
 client.connect( function(err) {
     if(err) {
-        return console.error('could not connect to postgres', err);
+        return console.error('could not connect to postgres: ', err);
     }
-    client.query('SELECT rm.* FROM ".SIERRA_record_metadata." INNER JOIN ".SIERRA_bib_view." ON rm.record_num = bv.record_num WHERE rm.record_last_updated_gmt > '".$date."' AND rm.record_type_code = 'b' AND bv.bcode3 = '-' ORDER BY rm.record_last_updated_gmt ASC;', function(err, result) {
+    client.query(query, [date, 'b', '-'], function(err, result) {
         if(err) {
             return console.error('error running query', err);
         }
-        console.log(result.rows[0].theTime);
+        console.log(result.rows);
         client.end();
     });
 });
